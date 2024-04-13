2e889ac874d84c11303f789b09f93d3c53a02943
Madlhawa <madhawa242@gmail.com>
2024-04-13 22:47:43 +0530

    added search

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/harithma'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
@@ -12,7 +15,11 @@ db = SQLAlchemy(app)
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    contact = db.Column(db.Integer)
+    contact = db.Column(db.String(10))
+    address = db.Column(db.String(255))
+    email = db.Column(db.String(150))
+    create_dttm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    update_dttm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     vehicals = db.relationship('Vehical', backref='owner', lazy=True)
 
     def __repr__(self):
@@ -24,27 +31,36 @@ class Vehical(db.Model):
     make = db.Column(db.String(20))
     model = db.Column(db.String(20))
     year = db.Column(db.String(4))
+    create_dttm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    update_dttm = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     owner_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
 
     def __repr__(self):
         return f"Vehical('{self.number}','{self.make}','{self.model}','{self.year}')"
 
-@app.route('/customer/')
+@app.route('/customer/', methods = ['GET', 'POST'])
 def customer():
-    customers = Customer.query.all()
-    return render_template('index.html', customers = customers)
+    if request.method == 'POST' and 'query' in request.form:
+        query = request.form["query"]
+        customers = Customer.query.filter(Customer.name.icontains(query)).all()
+        return render_template('customer.html',customers=customers, query=query)
+    else:
+        customers = Customer.query.all()
+        return render_template('customer.html',customers=customers)
 
 @app.route('/insert_customer/', methods = ['POST'])
 def insert_customer():
     if request.method == "POST":
         customer = Customer(
             name = request.form.get('name'),
-            contact = request.form.get('contact')
+            contact = request.form.get('contact'),
+            address = request.form.get('address'),
+            email = request.form.get('email')
         )
         db.session.add(customer)
         db.session.commit()
         flash("Customer added successfully!")
-        return redirect(url_for('index'))
+        return redirect(url_for('customer'))
     
 @app.route('/update_customer/', methods = ['POST'])
 def update_customer():
@@ -54,7 +70,7 @@ def update_customer():
         customer.contact = request.form['contact']
         db.session.commit()
         flash("Customer is updated!")
-        return redirect(url_for('index'))
+        return redirect(url_for('customer'))
     
 @app.route('/delete_customer/<id>', methods = ['GET', 'POST'])
 def delete_customer(id):
@@ -62,7 +78,7 @@ def delete_customer(id):
     db.session.delete(customer)
     db.session.commit()
     flash("Customer is deleted!")
-    return redirect(url_for('index'))
+    return redirect(url_for('customer'))
 
 @app.route('/insert_vehical/', methods = ['POST'])
 def insert_vehical():
@@ -77,7 +93,7 @@ def insert_vehical():
         db.session.add(vehical)
         db.session.commit()
         flash("Vehical added successfully!")
-        return redirect(url_for('index'))
+        return redirect(url_for('customer'))
 
 @app.route('/update_vehical/', methods = ['POST'])
 def update_vehical():
@@ -90,7 +106,7 @@ def update_vehical():
         vehical.owner_id = request.form['owner_id']
         db.session.commit()
         flash("Vehical is updated!")
-        return redirect(url_for('index'))
+        return redirect(url_for('customer'))
     
 @app.route('/delete_vehical/<id>', methods = ['GET', 'POST'])
 def delete_vehical(id):
@@ -98,7 +114,11 @@ def delete_vehical(id):
     db.session.delete(vehical)
     db.session.commit()
     flash("Vehical is deleted!")
-    return redirect(url_for('index'))
+    return redirect(url_for('customer'))
+
+@app.route('/test/')
+def test():
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
